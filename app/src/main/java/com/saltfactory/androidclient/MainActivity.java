e34aabf347e681962158efaafb1503154f60382e
Eurybus <wraitholyth@gmail.com>
2017-11-25 10:45:53 +0200

    Messing with permissions.

--- app/src/main/java/com/saltfactory/androidclient/MainActivity.java
+++ app/src/main/java/com/saltfactory/androidclient/MainActivity.java
@@ -3,21 +3,30 @@ package com.saltfactory.androidclient;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Build;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.PermissionChecker;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
 import com.google.android.gms.location.LocationRequest;
 
+import io.proximi.proximiiolibrary.ProximiioAPI;
+import io.proximi.proximiiolibrary.ProximiioGeofence;
+import io.proximi.proximiiolibrary.ProximiioListener;
+
 import static android.R.attr.targetSdkVersion;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ProximiioAPI proximiioAPI;
+    private static final String TAG = "ProximiioDemo";
     public static final String AUTH = "AUTH_KEY_HERE";
     private final int REQUEST_LOCATION = 1;
+    private final int REQUEST_BLUETOOTH = 2;
+    private final int REQUEST_BLUETOOTH_ADMIN = 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +34,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+//    @Override
+//    protected void onStart(){
+//        int hasLocationPermission = ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION);
+//    }
     //Checking location permission
     public boolean selfPermissionGranted(String permission){
         // For Android < Android M, self permissions are always granted.
@@ -62,11 +75,14 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+//        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         switch (requestCode) {
             case REQUEST_LOCATION:
                 if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     // accepted, start getting location information
+                    proximiioAPI.onRequestPermissionsResult(requestCode, permissions, grantResults);
                     Toast.makeText(this, "Location access granted", Toast.LENGTH_SHORT).show();
+                    Log.d(TAG, "Location access granted");
                 } else {
                     // denied
                     Toast.makeText(this, "Location access denied by the user!", Toast.LENGTH_SHORT).show();
@@ -75,6 +91,29 @@ public class MainActivity extends AppCompatActivity {
             default:
                 super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         }
+    }
+
+    public void startButtonClicked(View view){
+        // Create our Proximi.io listener
+        proximiioAPI = new ProximiioAPI(TAG, this);
+        proximiioAPI.setListener(new ProximiioListener() {
+            @Override
+            public void geofenceEnter(ProximiioGeofence geofence) {
+                Log.d(TAG, "Geofence enter: " + geofence.getName());
+            }
+
+            @Override
+            public void geofenceExit(ProximiioGeofence geofence, @Nullable Long dwellTime) {
+                Log.d(TAG, "Geofence exit: " + geofence.getName() + ", dwell time: " + String.valueOf(dwellTime));
+            }
+
+            @Override
+            public void loginFailed(LoginError loginError) {
+                Log.e(TAG, "LoginError! (" + loginError.toString() + ")");
+            }
+        });
+        proximiioAPI.setLogin("h4211@student.jamk.fi", "omena11");
+        proximiioAPI.setActivity(this);
     }
 
 }
